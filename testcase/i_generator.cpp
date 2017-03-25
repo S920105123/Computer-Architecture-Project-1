76c12c4b82444a9f4ad617f626bd51730492fa86
Benjamin <milk500ted939@gmail.com>
2017-03-25 16:56:35 +0800

    done

--- testcase/i_generator.cpp
+++ testcase/i_generator.cpp
@@ -22,9 +22,11 @@ int main()
 		cout<<"Eat "<<inst<<endl;
 		if (R_code.find(inst)!=R_code.end()) {
 			cin>>dec>>rs>>rt>>rd>>hex>>imm;
+		cout<<dec<<rs<<" "<<rt<<" "<<rd<<" "<<hex<<imm<<endl;
 			word=(rs<<21)|(rt<<16)|(rd<<11)|(imm<<6)|R_code[inst];
 		} else if (I_code.find(inst)!=I_code.end()) {
 			cin>>dec>>rs>>rt>>hex>>imm;
+			cout<<dec<<rs<<" "<<rt<<" "<<hex<<imm<<endl;
 			word=(I_code[inst]<<26)|(rs<<21)|(rt<<16)|(imm&0xFFFF);
 		} else if (J_code.find(inst)!=J_code.end()) {
 			cin>>hex>>imm;
